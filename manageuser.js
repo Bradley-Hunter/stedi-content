66d4b336280c8c9216b1fe75358c54d26ebe32da
Bradley Hunter <104086413+Bradley-Hunter@users.noreply.github.com>
2023-01-21 10:18:29 -0700

    followed along in class

--- manageuser.js
+++ manageuser.js
@@ -46,6 +46,18 @@ function checkexpiredtoken(token){
     }
 }
 
+function oneTimePassword(){
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://dev.stedi.me/twofactorlogin/9712039094',
+        data: JSON.stringify({userName, password}),
+        contentType: "application/text",
+        dataType: 'text'
+    });
+
+}
+
 function userlogin(){
     setuserpassword();
     setusername();
